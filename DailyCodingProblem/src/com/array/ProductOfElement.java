a8c71149d4ca6b07bae21efc88113dcadd66a86f
Rajender Thota <java.rajender@gmail.com>
2022-09-04 09:43:46 -0400

    Daily Coding Problem: Problem #2 [Hard]

---
+++ DailyCodingProblem/src/com/array/ProductOfElement.java
@@ -0,0 +1,56 @@
+package com.array;
+
+import java.util.Arrays;
+
+public class ProductOfElement {
+
+	/**
+	 * This problem was asked by Uber.
+	 * 
+	 * Given an array of integers, return a new array such that each element at
+	 * index i of the new array is the product of all the numbers in the original
+	 * array except the one at i.
+	 * 
+	 * For example, if our input was [1, 2, 3, 4, 5], the expected output would be
+	 * [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would
+	 * be [2, 3, 6].
+	 */
+	
+	public static void main(String args[]) {
+		
+		int arr[]= {1, 2, 3, 4, 5};
+		//displaying original input array
+		Arrays.stream(arr).forEach(e->{
+			System.out.print(""+e);
+		});
+		
+		System.out.println("\n");
+		//find product of existing array
+		Arrays.stream(productArray(arr)).forEach(e->{
+			System.out.print(" "+e);
+		});
+		
+	}
+	
+	//find product of given array
+	public static int[] productArray(int arr[]) {
+		
+		if( (arr.length-1 == 1 ) || (arr.length ==0))
+			return null;
+		
+		int ProductArr[]=new int[arr.length];
+		
+		for(int i=0;i<arr.length;i++) {
+			int product=1;
+			for(int j=0;j<arr.length;j++) {
+				if(i!=j)
+				product*=arr[j];
+			}
+			ProductArr[i]=product;
+			
+		}
+		
+		return ProductArr;
+	}
+
+}
